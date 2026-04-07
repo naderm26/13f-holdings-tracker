530816d67316b67ee2b98250840bd5c9c9985017
naderm26 <nadermassoudi26@gmail.com>
2026-04-06 21:51:33 -0700

    Update generate_pages.py

--- generate_pages.py
+++ generate_pages.py
@@ -225,6 +225,14 @@ def render_page(manager, live, all_managers):
     return f"""<!DOCTYPE html>
 <html lang="en">
 <head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2TBN6R2JZC"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-2TBN6R2JZC');
+</script>
   <meta charset="UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>{title}</title>
